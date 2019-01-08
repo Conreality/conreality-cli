7f343fadda773ac3dc5a770e1097f45e16cbc2c4
Arto Bendiken <arto@conreality.org>
2019-01-08 21:53:18 +0200

    Implemented `concli init`.

---
+++ concli/cmd/init.go
@@ -0,0 +1,36 @@
+/* This is free and unencumbered software released into the public domain. */
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// InitCmd describes and implements the `concli init` command
+var InitCmd = &cobra.Command{
+	Use:   "init",
+	Short: "Initialize configuration",
+	Long:  `This is the command-line interface (CLI) for Conreality.`,
+	Args:  cobra.NoArgs,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		configDirPath := fmt.Sprintf("%s/.conreality", os.Getenv("HOME"))
+
+		// Create the $HOME/.conreality directory, if it doesn't exist:
+		if err := os.Mkdir(configDirPath, 0700); err != nil && !os.IsExist(err) {
+			panic(err)
+		}
+
+		// Set the correct permissions on the $HOME/.conreality directory:
+		if err := os.Chmod(configDirPath, 0700); err != nil {
+			panic(err)
+		}
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(InitCmd)
+}
