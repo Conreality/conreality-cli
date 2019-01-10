c2ed3f5725ef022a8678711cbe7ce9f1a8296a67
Arto Bendiken <arto@conreality.org>
2019-01-10 09:50:17 +0200

    Enhanced `concli init` to also create $HOME/.conreality/games.

--- concli/cmd/init.go
+++ concli/cmd/init.go
@@ -18,6 +18,7 @@ var InitCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 
 		configDirPath := fmt.Sprintf("%s/.conreality", os.Getenv("HOME"))
+		gamesDirPath := fmt.Sprintf("%s/games", configDirPath)
 
 		// Create the $HOME/.conreality directory, if it doesn't exist:
 		if err := os.Mkdir(configDirPath, 0700); err != nil && !os.IsExist(err) {
@@ -26,6 +27,16 @@ var InitCmd = &cobra.Command{
 
 		// Set the correct permissions on the $HOME/.conreality directory:
 		if err := os.Chmod(configDirPath, 0700); err != nil {
+			panic(err)
+		}
+
+		// Create the $HOME/.conreality/games directory, if it doesn't exist:
+		if err := os.Mkdir(gamesDirPath, 0700); err != nil && !os.IsExist(err) {
+			panic(err)
+		}
+
+		// Set the correct permissions on the $HOME/.conreality/games directory:
+		if err := os.Chmod(gamesDirPath, 0700); err != nil {
 			panic(err)
 		}
 	},
